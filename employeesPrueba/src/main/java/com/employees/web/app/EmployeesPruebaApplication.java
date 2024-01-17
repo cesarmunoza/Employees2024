8455686a1d5bc29bb8f1e7a1daff09e6ccb260e8
cesar <cesare.munoz@gmail.com>
2024-01-16 23:17:47 -0500

    Primer commit

---
+++ employeesPrueba/src/main/java/com/employees/web/app/EmployeesPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.employees.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesPruebaApplication.class, args);
+	}
+
+}
