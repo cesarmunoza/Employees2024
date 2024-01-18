2e1d28b13da119256245fbb7aed54abf4a861cc3
cesar <cesare.munoz@gmail.com>
2024-01-18 07:44:48 -0500

    Se arregló la lógica del controlador para que mostrara los campos requeridos del empleado en la vista employee-details. Se arregló la vista para que mostrara el campo de texto y el botón de búsqueda.

--- employeesPrueba/src/main/java/com/employees/web/app/controller/EmployeeController.java
+++ employeesPrueba/src/main/java/com/employees/web/app/controller/EmployeeController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.employees.web.app.model.Employee;
 import com.employees.web.app.service.EmployeeSalary;
@@ -38,14 +38,24 @@ public class EmployeeController {
 		return "employee-list";
 	}
 	
-	@GetMapping("/employee/{id}")
-	public String getEmployee(@PathVariable("id") Long id, Model model) {
-		Employee employee = employeeService.getEmployeeById(id);
-		model.addAttribute("titulo", "Employee details");
-		model.addAttribute("employee", employee);
-		model.addAttribute("annualSalary", employeeSalary.calculateAnualSalary(employee.getEmployee_salary()));
-		return "employee-details";
+	@GetMapping("/employee-details")
+	public String getEmployeeDetails(@RequestParam(name = "id", required = false) Long id, Model model) {
+	    if (id != null) {
+	        Employee employee = employeeService.getEmployeeById(id);
+	        if (employee != null) {
+	            model.addAttribute("titulo", "Employee details");
+	            model.addAttribute("employee", employee);
+	            model.addAttribute("annualSalary", employeeSalary.calculateAnualSalary(employee.getEmployee_salary()));
+	            return "employee-details";
+	        }
+	    }
+
+	    model.addAttribute("titulo", "Employee not found");
+	    model.addAttribute("employee", null);
+	    model.addAttribute("annualSalary", null);
+	    return "employee-details";
 	}
+
 	
 	
 	
